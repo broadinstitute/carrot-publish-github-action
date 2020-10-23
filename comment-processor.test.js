9113611227875e35df7687a43e7fe2d176f1ac3b
Kevin Lydon <klydon@broadinstitute.org>
2020-10-23 12:38:45 -0400

    Test print

---
+++ comment-processor.test.js
@@ -0,0 +1,126 @@
+const processor = require("./comment-processor")
+const core = require('@actions/core');
+const github = require('@actions/github');
+const githubClient = require('./github-client');
+const pubsubClient = require('./pubsub-client');
+
+jest.mock('@actions/core');
+jest.mock('@actions/github');
+jest.mock('./github-client');
+jest.mock('./pubsub-client');
+
+test('process comment success', async () => {
+    github.context = {
+        payload: {
+            issue: {
+                pull_request: {
+                    url: 'https://example.com/repos/test_account/test_repo/pull/3'
+                },
+                number: 3
+            },
+            comment: {
+                body: "#carrot(Test test name, key_1, key2)",
+                user: {
+                    login: "example_user"
+                }
+            },
+            repository: {
+                full_name: "test_account/test_repo"
+            }
+        }
+    };
+
+    core.getInput.mockImplementation((name) => name);
+
+    const prInfo = {
+        head: {
+            sha: "734713bc047d87bf7eac9674765ae793478c50d3"
+        }
+    };
+    githubClient.getPRInfo.mockImplementation(async () => prInfo);
+
+    const expectedMessage = {
+        "source": "github",
+        "test_name": "Test test name",
+        "test_input_key": "key_1",
+        "eval_input_key": "key2",
+        "software_name": "software-name",
+        "commit": "734713bc047d87bf7eac9674765ae793478c50d3",
+        "owner": "test_account",
+        "repo": "test_repo",
+        "issue_number": 3,
+        "author": "example_user"
+    };
+    pubsubClient.publishMessage.mockImplementation(async (message) => {
+        expect(message).toBe(JSON.stringify(expectedMessage));
+        return 123456
+    });
+
+    console.log = jest.fn();
+
+    await processor.processComment();
+
+    expect(console.log).toHaveBeenCalledWith("Message sent with ID: 123456");
+});
+
+test('process comment not a pr', async () => {
+    github.context = {
+        payload: {
+            issue: {
+                number: 3
+            },
+            comment: {
+                body: "#carrot(name,key1,key2)",
+                user: {
+                    login: "example_user"
+                }
+            },
+            repository: {
+                full_name: "test_account/test_repo"
+            }
+        }
+    };
+
+    console.log = jest.fn();
+
+    pubsubClient.publishMessage.mockImplementation(async () => {
+        throw new Error("Published a message even though it shouldn't");
+    });
+
+    await processor.processComment();
+
+    expect(console.log).toHaveBeenCalledWith("Not a PR comment");
+});
+
+test('process comment not a carrot comment', async () => {
+    github.context = {
+        payload: {
+            issue: {
+                pull_request: {
+                    url: 'https://example.com/repos/test_account/test_repo/pull/3'
+                },
+                number: 3
+            },
+            comment: {
+                body: "Hi, everybody!",
+                user: {
+                    login: "example_user"
+                }
+            },
+            repository: {
+                full_name: "test_account/test_repo"
+            }
+        }
+    };
+
+    console.log = jest.fn();
+
+    pubsubClient.publishMessage.mockImplementation(async () => {
+        throw new Error("Published a message even though it shouldn't");
+    });
+
+
+    await processor.processComment();
+
+    expect(console.log).toHaveBeenCalledWith("Comment is not formatted as a CARROT command comment");
+});
