13a35b9290a5a32e4d5e60f12fdea9194d0bf6af
Kevin Lydon <klydon@broadinstitute.org>
2022-07-18 15:57:05 -0400

    Added support for PR (branch vs master) runs

--- comment-parser.test.js
+++ comment-parser.test.js
@@ -3,6 +3,17 @@ const parser = require('./comment-parser');
 test('parse comment with both keys', () => {
     const parsed = parser.parseComment('#carrot(Github actions test, in_test_image, in_eval_image)');
     expect(parsed).toStrictEqual({
+        runType: "run",
+        testName: "Github actions test",
+        testInputKey: "in_test_image",
+        evalInputKey: "in_eval_image"
+    });
+});
+
+test('parse comment for pr run', () => {
+    const parsed = parser.parseComment('#carrot_pr(Github actions test, in_test_image, in_eval_image)');
+    expect(parsed).toStrictEqual({
+        runType: "pr",
         testName: "Github actions test",
         testInputKey: "in_test_image",
         evalInputKey: "in_eval_image"
@@ -12,6 +23,7 @@ test('parse comment with both keys', () => {
 test('parse comment with test key', () => {
     const parsed = parser.parseComment('#carrot(test_name, test_key,)');
     expect(parsed).toStrictEqual({
+        runType: "run",
         testName: "test_name",
         testInputKey: "test_key",
         evalInputKey: ""
@@ -21,6 +33,7 @@ test('parse comment with test key', () => {
 test('parse comment with eval key', () => {
     const parsed = parser.parseComment('#carrot(test_name, , eval_key)');
     expect(parsed).toStrictEqual({
+        runType: "run",
         testName: "test_name",
         testInputKey: "",
         evalInputKey: "eval_key"
--- comment-processor.test.js
+++ comment-processor.test.js
@@ -37,21 +37,90 @@ test('process comment success', async () => {
     const prInfo = {
         head: {
             sha: "734713bc047d87bf7eac9674765ae793478c50d3"
+        },
+        base: {
+            sha: "19284054047d87bf7eac9674765ae793478c50d3"
         }
     };
     githubClient.getPRInfo.mockImplementation(async () => prInfo);
 
     const expectedMessage = {
-        "source": "github",
-        "test_name": "Test test name",
-        "test_input_key": "key_1",
-        "eval_input_key": "key2",
-        "software_name": "software-name",
-        "commit": "734713bc047d87bf7eac9674765ae793478c50d3",
-        "owner": "test_account",
-        "repo": "test_repo",
-        "issue_number": 3,
-        "author": "example_user"
+        "request_type": "run",
+        "body": {
+            "source": "github",
+            "test_name": "Test test name",
+            "test_input_key": "key_1",
+            "eval_input_key": "key2",
+            "software_name": "software-name",
+            "commit": "734713bc047d87bf7eac9674765ae793478c50d3",
+            "owner": "test_account",
+            "repo": "test_repo",
+            "issue_number": 3,
+            "author": "example_user"
+        }
+    };
+    pubsubClient.publishMessage.mockImplementation(async (message) => {
+        expect(message).toBe(JSON.stringify(expectedMessage));
+        return 123456
+    });
+
+    permissionsChecker.userHasPermission.mockImplementation(async () => true);
+
+    console.log = jest.fn();
+
+    await processor.processComment();
+
+    expect(console.log).toHaveBeenCalledWith("Message sent with ID: 123456");
+});
+
+test('process comment success pr', async () => {
+    github.context = {
+        payload: {
+            issue: {
+                pull_request: {
+                    url: 'https://example.com/repos/test_account/test_repo/pull/3'
+                },
+                number: 3
+            },
+            comment: {
+                body: "#carrot_pr(Test test name, key_1, key2)",
+                user: {
+                    login: "example_user"
+                }
+            },
+            repository: {
+                full_name: "test_account/test_repo"
+            }
+        }
+    };
+
+    core.getInput.mockImplementation((name) => name);
+
+    const prInfo = {
+        head: {
+            sha: "734713bc047d87bf7eac9674765ae793478c50d3"
+        },
+        base: {
+            sha: "19284054047d87bf7eac9674765ae793478c50d3"
+        }
+    };
+    githubClient.getPRInfo.mockImplementation(async () => prInfo);
+
+    const expectedMessage = {
+        "request_type": "pr",
+        "body": {
+            "source": "github",
+            "test_name": "Test test name",
+            "test_input_key": "key_1",
+            "eval_input_key": "key2",
+            "software_name": "software-name",
+            "head_commit": "734713bc047d87bf7eac9674765ae793478c50d3",
+            "base_commit": "19284054047d87bf7eac9674765ae793478c50d3",
+            "owner": "test_account",
+            "repo": "test_repo",
+            "issue_number": 3,
+            "author": "example_user"
+        }
     };
     pubsubClient.publishMessage.mockImplementation(async (message) => {
         expect(message).toBe(JSON.stringify(expectedMessage));
